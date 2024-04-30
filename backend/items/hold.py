818474b33d3b0d8f761cfab588225f6cea540a01
benphamroodman <bnroodman@gmail.com>
2024-04-30 10:41:40 +0800

    Feat: Added hold attribute xAtYMax to store the x value of the highest y value on the hold

--- backend/items/hold.py
+++ backend/items/hold.py
@@ -1,5 +1,5 @@
 class Hold:
-    def __init__(self, id, wall, location, color, end, yMax), xAtYMax:
+    def __init__(self, id, wall, location, color, end, yMax, xAtYMax):
         self.id = id
         self.wall = wall #this way we enforce a wall object to be connected to a hold
         self.location = location  # should be a 2D array
