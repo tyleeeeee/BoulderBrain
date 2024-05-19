9acb24c3132f0000fa60196b67112315f072468d
tyleeeeee <tyleeeeee815@gmail.com>
2024-05-20 00:12:19 +0800

    using holds extracted from image

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from services.image_processing_service import generate_dense_holds, get_holds_from_image
-from services.route_generation_service import generateRoutes
+from services.image_processing_service import get_holds_from_image, get_holds_main
+# from services.image_processing_service import generate_dense_holds, get_holds_from_image
+from services.route_generation_service import generateRoutes, process_final_routes, filter_routes_by_hold_overlap
 from services.reachable_foot_area import calc_knee_angle, calc_hold_angle, calc_hip_angle, calc_max_hip_angle
-
+from services.output_processing import output_route
 from services.climber import Climber
 from services.wall import Wall
+
 
 import random
 import copy
 
+#gggg
 app = Flask(__name__)
 CORS(app)
 
@@ -24,50 +27,39 @@ def data():
 @app.route('/api/generate_routes', methods=['GET'])
 def api_generate_routes():
     try:
+
         # Initialize Wall and Climber
-        newWall = Wall(id=1, height=400, width=500) #made it quite larger on purpose
-        newClimber = Climber(newWall, height=180, upper_arm_length=40, forearm_length=30,
+        image_path = 'services/files/example_wall.jpg'
+        print("Loading wall and climber details...")
+        wall = Wall(id=1, height=350, width=500, image_path=image_path) #made it quite larger on purpose
+        climber = Climber(wall, height=180, upper_arm_length=40, forearm_length=30,
                           upper_leg_length=45, lower_leg_length=40, torso_height=80,
                           torso_width=50)
 
-        routes = generateRoutes(newWall, newClimber)
-        return jsonify({"reply": "this is not correctly calling the right function! Better use backend terminal rn. "})
+        # Set up a new wall with holds
+        # wall.holds = get_holds_from_image(wall)
+        wall.holds,holds_map = get_holds_main(wall)
 
-    #     # # Set up a new wall with holds
-    #     # wall.holds = get_holds_from_image()
-    #     #
 
-    #     #new wall with dense holds
-    #     wall.holds = generate_dense_holds(wall)
+        print("Generating routes...")
+        routes = generateRoutes(wall, climber)
+        holds_dict, routes_description_dict = process_final_routes(routes)
+    
+        # overlap_threshold = 30  # TODO: adjust where? Frontend? Try again when tree grows longer
+        overlap_threshold = int(input("Insert your maximum overlap threshold: "))
+        # print("OT:", overlap_threshold )
+        print("Getting valid routes...")
+        valid_routes = filter_routes_by_hold_overlap(holds_dict, overlap_threshold, wall)
+        # print("Valid Routes:", valid_routes)    
+        # Convert set to list to make it serializable
+        valid_routes_list = list(valid_routes)
+        print(f"Total of {len(valid_routes_list)} valid routes.")
 
-    #     # generate routes
-    #     routes = generateRoutes(wall, climber)
+        output_route(wall.holds, holds_map, valid_routes, wall.image_path)
+        
+        # return jsonify(valid_routes)
+        return jsonify({'Valid Routes': valid_routes})
 
-    #     print("Number of routes generated: ", len(routes))
-
-    #     for position in routes: print(position.toString())
-
-    #     finalPosition = routes[random.randint(1, len(routes))]
-
-    #     finalRoute = [finalPosition.toString()]
-
-    #     currentPosition = finalPosition
-    #     parentPosition = currentPosition.parent_position
-    #     iteration = 0
-    #     while (currentPosition.parent_position != None):
-    #         iteration += 1
-    #         print("Iteration: ", iteration)
-    #         print("Current position:", currentPosition.toString())
-    #         print("Parent position: ", parentPosition.toString())
-    #         finalRoute.insert(0, currentPosition.parent_position.toString())
-    #         currentPosition = copy.deepcopy(parentPosition)
-    #         parentPosition = currentPosition.parent_position
-
-    #     # print(finalRoute)
-
-    #     if not routes:
-    #         raise ValueError("No routes could be generated with the current setup.")
-    #     return jsonify({"routes": routes}) # best for frontend
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
