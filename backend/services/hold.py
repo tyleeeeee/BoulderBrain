0de3b62eb6f6862fa68ab38a9b310c6e58801e70
tyleeeeee <tyleeeeee815@gmail.com>
2024-06-03 21:19:15 +0800

    keep random difficulty

--- backend/services/hold.py
+++ backend/services/hold.py
@@ -1,11 +1,12 @@
 class Hold:
-    def __init__(self, wall, location, color, end, yMax, difficulty_1, difficulty_2, difficulty_3, difficulty_4, difficulty_5, difficulty_6, difficulty_7, difficulty_8, id):
+    def __init__(self, wall, location, color, end, yMax, difficulty, difficulty_1, difficulty_2, difficulty_3, difficulty_4, difficulty_5, difficulty_6, difficulty_7, difficulty_8, id):
         
         self.wall = wall #this way we enforce a wall object to be connected to a hold
         self.location = location  # should be a 2D array
         self.color = color
         self.end = end
         self.yMax = yMax # should be a 2D array #TODO: use yMax as HoldID everywhere
+        self.difficulty = difficulty
         self.difficulty_1 = difficulty_1
         self.difficulty_2 = difficulty_2
         self.difficulty_3 = difficulty_3
