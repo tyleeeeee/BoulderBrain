4728209f6f77fe9bbf8c7d2bb6c323b4abb46565
benphamroodman <bnroodman@gmail.com>
2024-06-30 15:35:47 -0400

    Feat: Lots of thigns

--- backend/app.py
+++ backend/app.py
@@ -31,13 +31,13 @@ def api_generate_routes():
 
         # Initialize Wall and Climber
         # image_path = 'services/files/example_wall.jpg'
-        image_path = 'services/files/860.jpg'
+        image_path = 'services/files/example_wall.jpg'
         print("Loading wall and climber details...")
 
-        wall = Wall(id=4, height=350, width=450, image_path=image_path) #made it quite larger on purpose
-        climber = Climber(wall, height=180, upper_arm_length=40*(10/9), forearm_length=30*(10/9),
-                          upper_leg_length=45*(10/9), lower_leg_length=40*(10/9), torso_height=80*(10/9),
-                          torso_width=50*(10/9))
+        wall = Wall(id=9, height=400, width=150, image_path=image_path) #made it quite larger on purpose
+        climber = Climber(wall, height=180, upper_arm_length=40*(9/9), forearm_length=30*(9/9),
+                          upper_leg_length=45*(9/9), lower_leg_length=40*(9/9), torso_height=80*(9/9),
+                          torso_width=50*(9/9))
 
         # Set up a new wall with holds
         holds_path = f'services/result{wall.id}/holds'
@@ -48,19 +48,19 @@ def api_generate_routes():
 
         print("Generating routes...")
         routes = generateRoutes(wall, climber)
-        holds_dict, routes_description_dict = process_final_routes(routes)
+        holds_dict, routes_description_dict, route_difficulties_dict = process_final_routes(routes)
     
         # overlap_threshold = 30  # TODO: adjust where? Frontend? Try again when tree grows longer
         overlap_threshold = int(input("Insert your maximum overlap threshold: "))
         # print("OT:", overlap_threshold )
         print("Getting valid routes...")
-        valid_routes = filter_routes_by_hold_overlap(holds_dict, overlap_threshold, wall)
+        valid_routes, valid_difficulties = filter_routes_by_hold_overlap(holds_dict, overlap_threshold, wall, route_difficulties_dict)
         # print("Valid Routes:", valid_routes)    
         # Convert set to list to make it serializable
         valid_routes_list = list(valid_routes)
-        print(f"Total of {len(valid_routes_list)} valid routes.")
+        print(f"Total of {len(valid_routes)} valid routes.")
 
-        output_route(wall.holds, holds_map, valid_routes, wall.image_path, files_path)
+        output_route(wall.holds, holds_map, valid_routes, valid_difficulties, wall.image_path, files_path)
         
         # return jsonify(valid_routes)
         return jsonify({'Valid Routes': valid_routes})
